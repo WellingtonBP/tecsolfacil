fd18593f8457645362673efa11b35bd82ed59520
WellingtonBP <wellingtonpacheco14@gmail.com>
2022-04-12 15:58:52 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tecsolfacil
+
+**TODO: Add description**
+
---
+++ apps/jobs/lib/jobs.ex
@@ -0,0 +1,18 @@
+defmodule Jobs do
+  @moduledoc """
+  Documentation for `Jobs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Jobs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
