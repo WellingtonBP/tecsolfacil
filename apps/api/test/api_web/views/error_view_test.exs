d843ab352e8e0f764ebfa8261741f88555adf453
WellingtonBP <wellingtonpacheco14@gmail.com>
2022-04-14 04:08:49 -0300

    add views test

--- apps/api/test/api_web/views/error_view_test.exs
+++ apps/api/test/api_web/views/error_view_test.exs
@@ -1,4 +1,17 @@
 defmodule ApiWeb.ErrorViewTest do
   use ApiWeb.ConnCase, async: true
 
+  import Phoenix.View
+
+  alias ApiWeb.ErrorView
+
+  describe "render/2" do
+    test "renders errors.json" do
+      assert render(ErrorView, "errors.json", %{errors: []}) == %{errors: []}
+    end
+
+    test "renders message.json" do
+      assert render(ErrorView, "message.json", %{message: "message"}) == %{message: "message"}
+    end
+  end
 end
---
+++ apps/api/test/api_web/views/zip_view_test.exs
@@ -0,0 +1,24 @@
+defmodule ApiWeb.ZipViewTest do
+  use ApiWeb.ConnCase, async: true
+
+  import Phoenix.View
+
+  alias ApiWeb.ZipView
+  alias Api.ZipCode.Info
+
+  describe "render/2" do
+    test "renders zip.json" do
+      zip_info = %Info{}
+      response =
+        zip_info
+        |> Map.from_struct()
+        |> Map.drop([:__meta__, :inserted_at, :updated_at, :id])
+
+      assert render(ZipView, "zip.json", %{zip: zip_info}) == response
+    end
+
+    test "renders csv.json" do
+      assert render(ZipView, "csv.json", %{}) == %{message: "csv sent to your email"}
+    end
+  end
+end
