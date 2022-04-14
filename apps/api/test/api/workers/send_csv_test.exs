086231cd9d1bf35fa0e1a04fec36b3489c1cc485
WellingtonBP <wellingtonpacheco14@gmail.com>
2022-04-14 03:42:14 -0300

    add sendcsv worker module tests

---
+++ apps/api/test/api/workers/send_csv_test.exs
@@ -0,0 +1,17 @@
+defmodule Api.Worker.SendCsvTest do
+  use Api.DataCase, async: true
+  use Oban.Testing, repo: Api.Repo
+
+  import Swoosh.TestAssertions
+
+  alias Api.Worker.SendCsv
+
+  describe "perform/1" do
+    test "should create csv and sent to email" do
+      System.put_env("EMAIL", "sender@email.com")
+
+      assert {:ok, _} = perform_job(SendCsv, %{email: "test@email.com"})
+      assert_email_sent(subject: "Addresses CSV File")
+    end
+  end
+end
